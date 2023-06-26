a4c164db029648b4628b5cda45cf753f96bb3a94
irysja <irysja.gyikova@gmail.com>
2023-06-26 20:56:37 +0300

    work on header

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './js/service/API';
 import './js/utils/slider';
 import './js/utils/scrollbar';
 import './js/renders/renders.js';
+import './js/utils/localSctorage.js';
+import './js/utils/mobile-menu.js'
