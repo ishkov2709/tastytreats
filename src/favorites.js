5b9bbd8b40180500b6fd409b5aef04980e5fcb04
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-06-27 00:43:07 +0300

    fixed menu and header

--- src/favorites.js
+++ src/favorites.js
@@ -1,1 +1,3 @@
 import './js/utils/localSctorage';
+import './js/utils/switchTheme.js';
+import './js/utils/mobile-menu.js';
--- src/js/utils/localSctorage.js
+++ src/js/utils/localSctorage.js
@@ -1,19 +1,1 @@
-const themeSwitch = document.getElementById('themeSwitch');
-const body = document.body;
 
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  body.classList.add(savedTheme);
-}
-
-themeSwitch.addEventListener('click', () => {
-  if (body.classList.contains('light')) {
-    body.classList.remove('light');
-    body.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    body.classList.remove('dark');
-    body.classList.add('light');
-    localStorage.setItem('theme', 'light');
-  }
-});
