7590c1cba9d01958b0930176f1333980aa7cec9f
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-06-30 16:00:27 +0300

    Update search.js

--- src/js/renders/search.js
+++ src/js/renders/search.js
@@ -7,7 +7,7 @@ import startPagination from '../utils/pagination';
 import { changeThemePagination } from '../utils/switchTheme';
 import { OpenModal } from '../utils/modal-recipes';
 import { setActiveClass } from '../utils/scrollbar';
-import { fetchAreaRecipes , fetchIngredientsRecipes } from '../service/API.js'
+import { fetchAreaRecipes, fetchIngredientsRecipes } from '../service/API.js';
 
 // Refs
 
@@ -15,11 +15,10 @@ const searchInput = document.querySelector('.search-input');
 export const recipeContainer = document.querySelector('#image-container');
 const paginationBox = document.getElementById('pagination');
 const spinner = document.getElementById('spinner');
-const areaSelect = document.querySelector('.list-area')
-const ingredSelect = document.querySelector('.list-ingred')
-const filtersSection = document.querySelector('.input-section')
-const timeSelect = document.querySelector('.list-time')
-
+const areaSelect = document.querySelector('.list-area');
+const ingredSelect = document.querySelector('.list-ingred');
+const filtersSection = document.querySelector('.input-section');
+const timeSelect = document.querySelector('.list-time');
 
 // Vars
 
@@ -27,12 +26,9 @@ let prevSearch = '';
 let searchQuery = '';
 
 let query = '';
-let time = "";
+let time = '';
 let ingredient = '';
 let area = '';
-
-
-
 
 const DEBOUNCE_DELAY = 300;
 
@@ -67,11 +63,6 @@ searchImagesAndDisplay();
 // function hideSpinner() {
 //   spinner.style.display = 'none';
 // }
-
-
-
-
-
 
 // export async function searchImagesAndDisplay(
 //   currentPage = 1,
@@ -150,10 +141,7 @@ export function hendleClickOnRecipes({ target }) {
 
 // searchInput.addEventListener('input', debouncedHandleSearch);
 
-
 recipeContainer.addEventListener('click', hendleClickOnRecipes);
-
-
 
 /**
   |============================
@@ -163,57 +151,47 @@ recipeContainer.addEventListener('click', hendleClickOnRecipes);
 
 async function generatAreaFiltersMarkup() {
   try {
-    const filtersArea = await fetchAreaRecipes()
-  return filtersArea.reduce((markup, ivent) => 
-    markup + createAreaMarkupFilters(ivent), ''
-  );
-  } catch {
-    
-  }
+    const filtersArea = await fetchAreaRecipes();
+    return filtersArea.reduce(
+      (markup, ivent) => markup + createAreaMarkupFilters(ivent),
+      ''
+    );
+  } catch {}
 }
 
 async function generateIngredFiltersMarkup() {
-    try {
-      const filtersIngred = await fetchIngredientsRecipes()
-    
-  return filtersIngred.reduce((markup, ivent) => 
-    markup + createIngredMarkupFilters(ivent), ''
-  );
-  } catch {
-    
-  }
-}
+  try {
+    const filtersIngred = await fetchIngredientsRecipes();
 
+    return filtersIngred.reduce(
+      (markup, ivent) => markup + createIngredMarkupFilters(ivent),
+      ''
+    );
+  } catch {}
+}
 
 function createAreaMarkupFilters(ivent) {
   const { name } = ivent;
-  return `<option value="${name}">${name}</option>`
+  return `<option value="${name}">${name}</option>`;
 }
 
 function createIngredMarkupFilters(ivent) {
   const { name, _id } = ivent;
-  return `<option value="${_id}">${name}</option>`
+  return `<option value="${_id}">${name}</option>`;
 }
 
 async function createAreaFilters() {
   try {
-    const areasMarkup = await generatAreaFiltersMarkup()
-    addAreaFilters(areasMarkup)
-  } catch {
-
-  }
-   
+    const areasMarkup = await generatAreaFiltersMarkup();
+    addAreaFilters(areasMarkup);
+  } catch {}
 }
 
 async function createIngredFilters() {
-    try {
-    const ingredMarkup = await generateIngredFiltersMarkup()
-    addIngridientsFilters(ingredMarkup)
-  } catch {
-
-  }
-   
-
+  try {
+    const ingredMarkup = await generateIngredFiltersMarkup();
+    addIngridientsFilters(ingredMarkup);
+  } catch {}
 }
 function addAreaFilters(markup) {
   areaSelect.insertAdjacentHTML('beforeend', markup);
@@ -225,31 +203,32 @@ function addIngridientsFilters(markup) {
 
 const debouncedOnInpit = debounce(onInput, DEBOUNCE_DELAY);
 
-filtersSection.addEventListener('input', debouncedOnInpit)
+filtersSection.addEventListener('input', debouncedOnInpit);
 
 function onInput(e) {
-   const value = e.target.value
+  const value = e.target.value;
   if (e.target.classList.contains('search-input')) {
-    if(!value) return (searchInput.value = '')
+    if (!value) return (searchInput.value = '');
 
     query = customizeText(value);
-  } if (e.target.classList.contains('list-area')) {
+  }
+  if (e.target.classList.contains('list-area')) {
     area = value;
-  } if (e.target.classList.contains('list-ingred')) {
+  }
+  if (e.target.classList.contains('list-ingred')) {
     ingredient = value;
-  } if (e.target.classList.contains('list-time')) {
+  }
+  if (e.target.classList.contains('list-time')) {
     time = value;
   }
-  
+
   recipeContainer.innerHTML = '';
 
-
   setActiveClass();
-  searchImagesAndDisplay()
- 
-  console.log(query, area, time, ingredient)
-}
+  searchImagesAndDisplay();
 
+  console.log(query, area, time, ingredient);
+}
 
 // function handleSearch({ target }) {
 //   if (!target.value.trim()) return (searchInput.value = '');
@@ -273,11 +252,6 @@ function hideSpinner() {
   spinner.style.display = 'none';
 }
 
-
-
-
-
-
 export async function searchImagesAndDisplay(
   currentPage = 1,
   callback = searchOnTitle
@@ -286,11 +260,13 @@ export async function searchImagesAndDisplay(
     showSpinner();
     createAreaFilters();
     createIngredFilters();
-  
 
     const { page, perPage, totalPages, results } = await callback(
       query,
-      currentPage, time, area, ingredient
+      currentPage,
+      time,
+      area,
+      ingredient
     );
     if (!results.length) throw new Error('No result');
     const recipes = await [
@@ -310,30 +286,28 @@ export async function searchImagesAndDisplay(
 
     prevSearch = query;
   } catch (error) {
-    timeSelect.firstElementChild.setAttribute('selected', 'selected')
-    areaSelect.firstElementChild.setAttribute('selected', 'selected')
-    ingredSelect.firstElementChild.setAttribute('selected', 'selected')
+    timeSelect.firstElementChild.setAttribute('selected', 'selected');
+    areaSelect.firstElementChild.setAttribute('selected', 'selected');
+    ingredSelect.firstElementChild.setAttribute('selected', 'selected');
     paginationBox.style.display = 'none';
     Notiflix.Notify.warning('No result for your request, please try again!');
 
-
     query = '';
-    time = "";
+    time = '';
     ingredient = '';
     area = '';
-    
+
     if (prevSearch === searchQuery) prevSearch = '';
 
     prevSearch ? (searchQuery = prevSearch) : (searchQuery = '');
-
 
     // return;
     prevSearch ? (query = prevSearch) : (query = '');
     searchImagesAndDisplay();
   } finally {
-      timeSelect.firstElementChild.removeAttribute('selected', 'selected')
-    areaSelect.firstElementChild.removeAttribute('selected', 'selected')
-    ingredSelect.firstElementChild.removeAttribute('selected', 'selected')   
+    timeSelect.firstElementChild.removeAttribute('selected', 'selected');
+    areaSelect.firstElementChild.removeAttribute('selected', 'selected');
+    ingredSelect.firstElementChild.removeAttribute('selected', 'selected');
 
     hideSpinner();
     changeThemePagination();
@@ -358,15 +332,13 @@ function toggleFavriteRecipe(currentBtn) {
   }
 }
 
-function hendleClickOnRecipes({ target }) {
-  if (!target.closest('button')) return;
-  const currentBtn = target.closest('button');
-  if (currentBtn.name === 'favorite') {
-    toggleFavriteRecipe(currentBtn);
-  }
-  if (currentBtn.name === 'details') {
-    OpenModal(currentBtn);
-  }
-}
-
-
+// function hendleClickOnRecipes({ target }) {
+//   if (!target.closest('button')) return;
+//   const currentBtn = target.closest('button');
+//   if (currentBtn.name === 'favorite') {
+//     toggleFavriteRecipe(currentBtn);
+//   }
+//   if (currentBtn.name === 'details') {
+//     OpenModal(currentBtn);
+//   }
+// }
